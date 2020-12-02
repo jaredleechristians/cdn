062cf8526aae01b1fad4cd7aad6fc5ac10cd6fb9
Jared Christians <jaredleechristians@gmail.com>
2020-12-02 14:43:45 +0200

    Add files via upload

--- things-csv-export.js
+++ things-csv-export.js
@@ -1,6 +1,5 @@
 function csv(device,keys){
-
-    // any kind of extension (.txt,.cpp,.cs,.bat)
+    var token = "Bearer ";
     var filename = "";
     var name = "";
     var content = "";
@@ -8,57 +7,77 @@ function csv(device,keys){
     var now = date.getTime();
 
 
+    let xhr = new XMLHttpRequest();
 
-    $.ajaxSetup({
-        headers:{
-            'Content-Type': "application/json",
-            'X-Authorization': "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqYXJlZEBzZmlncm91cC5jby56YSIsInNjb3BlcyI6WyJURU5BTlRfQURNSU4iXSwidXNlcklkIjoiYzk1OTFmZTAtMjQzNS0xMWViLWE2ZjAtMjU4OGU5MDdjODVkIiwiZmlyc3ROYW1lIjoiSmFyZWQiLCJsYXN0TmFtZSI6IkNocmlzdGlhbnMiLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiMTc2OGVlZjAtMjM1NC0xMWViLWE2ZjAtMjU4OGU5MDdjODVkIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNjA2ODE1MTkzLCJleHAiOjE2MDY4MjQxOTN9.DKH3X0IvqSJEPb9dXip8c-KHefPZwSAHn4THRrfwMf-AaLWEvohp5-v3pzOkgBA3dClXPDMeB7qOtxihG9ZUKQ"
-        }
-    });
-    
-    var url = "https://things.sfigroup.co.za/api/device/"+device;
-    $.get(url, function(data){
-        name = data.name;
-        filename = name+".csv";
+    // Set the request URL and request method
+    xhr.open("POST", "https://things.sfigroup.co.za/api/auth/login");
+
+    // Set the `Content-Type` Request header
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Accept", "application/json");
+
+    // Send the requst with Data
+    xhr.send('{"username":"jared@sfigroup.co.za", "password":"SfiJc001$"}');
+
+    xhr.onreadystatechange = function() {
+        // 	Check if request is completed
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            //	Do what needs to be done here
+            token += JSON.parse(xhr.response).token;
+
+            $.ajaxSetup({
+                headers:{
+                    'Content-Type': "application/json",
+                    'X-Authorization': token
+                }
+            });        
         
-    });
-
-    if (keys == 'temp'){
-        var url = "https://things.sfigroup.co.za/api/plugins/telemetry/DEVICE/"+device+"/values/timeseries?keys="+keys+"&startTs=0&endTs="+now+"&interval=60000&agg=NONE&limit=50000";
-        $.get(url, function(data){
-
-            for (var key in data.temp){
-                var date = new Date(data.temp[key].ts).toLocaleDateString("en-ZA");
-                var time = new Date(data.temp[key].ts).toLocaleTimeString("en-ZA");
-                //document.write('"'+name+'"'+";"+'"'+date + ' ' + time +'";"'+data.temp[key].value+'"'+"<br>");
-                content += name+';'+data.temp[key].value+';'+date + ' ' + time+'\n';
-            }
-
-            var blob = new Blob([content], {
-                type: "text/plain;charset=utf-8"
+            var url = "https://things.sfigroup.co.za/api/device/"+device;
+            $.get(url, function(data){
+                name = data.name;
+                filename = name+".csv";
+                
             });
 
-            saveAs(blob, filename);
-        });
-    }
-    
-    if (keys == 'humid'){
-        var url = "https://things.sfigroup.co.za/api/plugins/telemetry/DEVICE/"+device+"/values/timeseries?keys="+keys+"&startTs=0&endTs="+now+"&interval=60000&agg=NONE&limit=50000";
-        $.get(url, function(data){
+            if (keys == 'temp'){
+                var url = "https://things.sfigroup.co.za/api/plugins/telemetry/DEVICE/"+device+"/values/timeseries?keys="+keys+"&startTs=0&endTs="+now+"&interval=60000&agg=NONE&limit=50000";
+                $.get(url, function(data){
 
-            for (var key in data.humid){
-                var date = new Date(data.humid[key].ts).toLocaleDateString("en-ZA");
-                var time = new Date(data.humid[key].ts).toLocaleTimeString("en-ZA");
-                //document.write('"'+name+'"'+";"+'"'+date + ' ' + time +'";"'+data.humid[key].value+'"'+"<br>");
-                content += name+';'+data.humid[key].value+';'+date + ' ' + time+'\n';
+                    for (var key in data.temp){
+                        var date = new Date(data.temp[key].ts).toLocaleDateString("en-ZA");
+                        var time = new Date(data.temp[key].ts).toLocaleTimeString("en-ZA");
+                        //document.write('"'+name+'"'+";"+'"'+date + ' ' + time +'";"'+data.temp[key].value+'"'+"<br>");
+                        content += name+';'+data.temp[key].value+';'+date + ' ' + time+'\n';
+                    }
+
+                    var blob = new Blob([content], {
+                        type: "text/plain;charset=utf-8"
+                    });
+
+                    saveAs(blob, filename);
+                });
             }
+            
+            if (keys == 'humid'){
+                var url = "https://things.sfigroup.co.za/api/plugins/telemetry/DEVICE/"+device+"/values/timeseries?keys="+keys+"&startTs=0&endTs="+now+"&interval=60000&agg=NONE&limit=50000";
+                $.get(url, function(data){
 
-            var blob = new Blob([content], {
-                type: "text/plain;charset=utf-8"
-            });
+                    for (var key in data.humid){
+                        var date = new Date(data.humid[key].ts).toLocaleDateString("en-ZA");
+                        var time = new Date(data.humid[key].ts).toLocaleTimeString("en-ZA");
+                        //document.write('"'+name+'"'+";"+'"'+date + ' ' + time +'";"'+data.humid[key].value+'"'+"<br>");
+                        content += name+';'+data.humid[key].value+';'+date + ' ' + time+'\n';
+                    }
 
-            saveAs(blob, filename);
-        });
+                    var blob = new Blob([content], {
+                        type: "text/plain;charset=utf-8"
+                    });
+
+                    saveAs(blob, filename);
+                });
+            }
+        }
+
     }
 
 }
